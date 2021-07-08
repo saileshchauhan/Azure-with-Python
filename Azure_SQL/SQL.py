27029c712af57561af9a794da91bc81fccf114d6
saileshchauhan <chauhansailesh68@gmail.com>
2021-07-08 18:17:20 +0530

    [sailesh] Add: file title

---
+++ Azure_SQL/SQL.py
@@ -0,0 +1,7 @@
+'''
+@Author: Sailesh Chauhan
+@Date: 08-07-2021
+@Last Modified time: 08-07-2021
+@Title : This Script provide method for CRUD operation in SQL Database table using
+         methods of class AzureSQL
+'''
