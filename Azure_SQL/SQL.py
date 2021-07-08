985e46b22ba56cab684d6349efcf44b022d705c0
saileshchauhan <chauhansailesh68@gmail.com>
2021-07-08 18:19:16 +0530

    [sailesh] Add: constructor for connection value

--- Azure_SQL/SQL.py
+++ Azure_SQL/SQL.py
@@ -12,3 +12,26 @@ import sys
 sys.path.append('D:\AzureWithPython\logConfig.py')
 import logConfig
 import logging
+
+class AzureSQL:
+    '''
+    Functions:
+        1.__init__(constructor)
+        2.create_table()
+        3.retrieve_rows()
+        4.insert_values()
+        5.update_record()
+        6.delete_record()
+
+    '''
+    def __init__(self):
+        self.db=pyodbc.connect(
+            Driver=config('driver'),
+            Server=config('server_name'),
+            Database=config('database_name'),
+            Uid=config('user_name'),
+            Pwd=config('password'),
+            Encrypt='yes',
+            TrustServerCertificate='no',
+            Connection_Timeout=30
+            )
